f1a195175e1acbddef6a25ddfb76471bddeb0be8
Raphael Cohn <raphael.cohn@stormmq.com>
2020-05-01 13:49:19 +0100

    copyright

--- src/erased_boxed_functions/Register.rs
+++ src/erased_boxed_functions/Register.rs
@@ -1,5 +1,5 @@
 // This file is part of message-dispatch. It is subject to the license terms in the COPYRIGHT file found in the top-level directory of this distribution and at https://raw.githubusercontent.com/lemonrock/message-dispatch/master/COPYRIGHT. No part of message-dispatch, including this file, may be copied, modified, propagated, or distributed except according to the terms contained in the COPYRIGHT file.
-// Copyright © 2019 The developers of message-dispatch. See the COPYRIGHT file in the top-level directory of this distribution and at https://raw.githubusercontent.com/lemonrock/message-dispatch/master/COPYRIGHT.
+// Copyright © 2019-2020 The developers of message-dispatch. See the COPYRIGHT file in the top-level directory of this distribution and at https://raw.githubusercontent.com/lemonrock/message-dispatch/master/COPYRIGHT.
 
 
 /// Allows one to register functions to handle objects.
